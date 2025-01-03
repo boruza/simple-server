1b04552e1667cb837990839814f71f969a95fb9a
boruza <abduba165@gmail.com>
2025-01-04 01:34:12 +0300

    Updated server configuration to use bcryptjs and removed start script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const multer = require("multer");
 const path = require("path"); // Import path module early
